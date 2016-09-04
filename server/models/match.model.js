328ec4609f0ad28c4c124083a01737cf41916135
Kirkhammetz <Neofrascati@hotmail.it>
2016-09-04 19:43:31 +0200

    match model tests

--- server/models/match.model.js
+++ server/models/match.model.js
@@ -1,15 +1,60 @@
 const mongoose = require('mongoose')
 
 const matchSchema = mongoose.Schema({
-  season: { type: mongoose.Schema.ObjectId, ref: 'season' },
-  round: { type: mongoose.Schema.ObjectId, ref: 'round' },
-  day: { type: mongoose.Schema.ObjectId, ref: 'day' },
-  teamHome: { type: mongoose.Schema.ObjectId, ref: 'team' },
-  teamAway: { type: mongoose.Schema.ObjectId, ref: 'team' },
-  winner: { type: mongoose.Schema.ObjectId, ref: 'team' },
-  loser: { type: mongoose.Schema.ObjectId, ref: 'team' },
-  played: { type: Boolean, default: false },
-  date: { type: Date, required: [true, 'Match date is required'] },
+  season: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'season',
+    required: [true, 'Season id is required'],
+  },
+  round: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'round',
+    required: [true, 'Round id is required'],
+  },
+  day: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'day',
+    required: [true, 'Day id is required'],
+  },
+  teamHome: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'team',
+    required: [true, 'Team at home id is required'],
+  },
+  teamAway: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'team',
+    required: [true, 'Team Away id is required'],
+  },
+  winner: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'team',
+  },
+  loser: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'team',
+  },
+  played: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    required: [true, 'Match date is required'],
+  },
+})
+
+matchSchema.pre('validate', function matchPreValidation(next) {
+  const match = this;
+  if (match.isModified('teamHome') || match.isModified('teamAway')) {
+    if (match.teamHome && match.teamAway) {
+      if (match.teamHome.equals(match.teamAway)) {
+        const err = new Error('match.model validation: teamHome and teamAway ids must be different!')
+        next(err)
+      }
+    }
+  }
+  next()
 })
 
 module.exports = mongoose.model('match', matchSchema, 'matchs')
---
+++ test/match/match.model.test.js
@@ -0,0 +1,166 @@
+const { testenv, getRandomInt, Promise } = global
+var Team = require(testenv.serverdir + 'models/team.model')
+var Season = require(testenv.serverdir + 'models/season.model')
+var Round = require(testenv.serverdir + 'models/round.model')
+var Day = require(testenv.serverdir + 'models/day.model')
+var Match = require(testenv.serverdir + 'models/match.model')
+var chai = require('chai')
+var expect = require('expect')
+
+describe('Match - Model', () => {
+  let seasonId, roundId, dayId, teamAId, teamBId
+
+  before((done) => {
+    Promise
+      .resolve(Season.create({ year: getRandomInt(3999,9999) }))
+      .then((season) => {
+        seasonId = season._id
+        return Round.create({
+          season: season._id,
+          label: 'Z',
+        })
+      })
+      .then((round) => {
+        roundId = round._id
+        return Day.create({
+          season: seasonId,
+          round: round._id
+        })
+      })
+      .then((day) => {
+        dayId = day._id
+        return Team.insertMany([
+          {
+            name: 'TeamA',
+            season: seasonId,
+            round: roundId,
+          },{
+            name: 'TeamB',
+            season: seasonId,
+            round: roundId,
+          },
+        ])
+
+      })
+      .then((teams) => {
+        teamAId = teams[0]._id
+        teamBId = teams[1]._id
+        done()
+      })
+      .catch(done)
+  })
+
+
+  it('should not create a match without a date', (done) => {
+    Match.create({}, (err) => {
+      expect(err).toExist()
+      expect(err.errors.date).toExist()
+      done()
+    })
+  })
+
+  it('should not create a match without season id', (done) => {
+    Match.create({
+      date: Date.now(),
+    }, (err) => {
+      expect(err).toExist()
+      expect(err.errors.season).toExist()
+      done()
+    })
+  })
+
+  it('should not create a match without round id', (done) => {
+    Match.create({
+      date: Date.now(),
+      season: seasonId,
+    }, (err) => {
+      expect(err).toExist()
+      expect(err.errors.round).toExist()
+      done()
+    })
+  })
+
+  it('should not create a match without day id', (done) => {
+    Match.create({
+      date: Date.now(),
+      season: seasonId,
+      round: roundId
+    }, (err) => {
+      expect(err).toExist()
+      expect(err.errors.day).toExist()
+      done()
+    })
+  })
+
+  it('should not create a match without a team at home id', (done) => {
+    Match.create({
+      date: Date.now(),
+      season: seasonId,
+      round: roundId,
+      day: dayId,
+      teamAway: teamBId,
+    }, (err) => {
+      expect(err).toExist()
+      expect(err.errors.teamHome).toExist()
+      done()
+    })
+  })
+
+  it('should not create a match without a team away id', (done) => {
+    Match.create({
+      date: Date.now(),
+      season: seasonId,
+      round: roundId,
+      day: dayId,
+      teamHome: teamAId,
+    }, (err) => {
+      expect(err).toExist()
+      expect(err.errors.teamAway).toExist()
+      done()
+    })
+  })
+
+  it('should not create a match if the two team id are equal', (done) => {
+    Match.create({
+      date: Date.now(),
+      season: seasonId,
+      round: roundId,
+      day: dayId,
+      teamHome: teamAId,
+      teamAway: teamAId,
+    }, (err) => {
+      expect(err).toExist()
+      expect(err.message).toExist()
+      done()
+    })
+  })
+
+  it('shoud finally create a match :)', (done) => {
+    Match.create({
+      date: Date.now(),
+      season: seasonId,
+      round: roundId,
+      day: dayId,
+      teamHome: teamAId,
+      teamAway: teamBId,
+    }, (err, match) => {
+      expect(err).toNotExist()
+      expect(match).toExist()
+      console.log(match);
+      done()
+    })
+  })
+
+  /**
+   * CLEANUP
+   */
+  after((done) => {
+    Promise.all([
+      Season.remove({ _id: seasonId }),
+      Round.remove({ _id: roundId }),
+      Day.remove({ _id: dayId }),
+      Team.remove({ _id: { $in: [teamAId, teamBId] } }),
+      Match.remove({}),
+    ]).then(done()).catch(done)
+  })
+})
