512fb38ab47ee0e76186887f8ac01125232d4bd9
Kirkhammetz <Neofrascati@hotmail.it>
2016-09-07 14:51:58 +0200

    settings

--- server/models/setting.model.js
+++ server/models/setting.model.js
@@ -4,8 +4,6 @@ const SettingShema = mongoose.Schema({
   sitename: { type: String, default: 'MyReactReduxPortfolio' },
   joinAllowed: { type: Boolean, default: false },
   mailContact: { type: String, default: 'mail@example.com' },
-}, {
-  capped: { max: 1 },
 })
 
 module.exports = mongoose.model('Setting', SettingShema)
