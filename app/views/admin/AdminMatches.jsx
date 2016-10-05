49e748c44983a48c03cee3772106e9a70aaeeb4c
Kirkhammetz <Neofrascati@hotmail.it>
2016-10-05 18:04:54 +0200

    cleaning

--- app/views/admin/AdminMatches.jsx
+++ app/views/admin/AdminMatches.jsx
@@ -10,8 +10,6 @@ import {
 import Callout from 'Callout'
 import MatchesList from 'MatchesList'
 
-// require('!style!css!sass!app/styles/admin/single-team.scss')
-
 class AdminMatches extends React.Component {
   constructor(props) {
     super(props)
@@ -21,21 +19,6 @@ class AdminMatches extends React.Component {
     const { dispatch } = this.props
     dispatch(clearAdminMatches())
   }
-
-  // renderPlayerLists() {
-  //   const { team } = this.props.team
-  //   const { dispatch } = this.props
-  //   if (team && team.players.length) {
-  //     return team.players.map((player, i) => {
-  //       return (
-  //         <div className="col-sm-12 col-md-4 single-player-badge" key={i}>
-  //           <TeamSinglePlayer player={player} dispatch={dispatch} />
-  //         </div>
-  //       )
-  //     })
-  //   }
-  //   return null
-  // }
 
   render() {
     const {
